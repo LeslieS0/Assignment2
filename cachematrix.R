f676d576f4c29464c0545d828ef3c2456c5114e1
LeslieS0 <32416954+LeslieS0@users.noreply.github.com>
2018-07-08 13:26:27 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,26 @@
-makeCacheMatrix <- function(x) {
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
+#Create special list containing four functions
+#Ensure matrix is invertible
+makeCacheMatrix <- function(x=matrix()) {
+  if(dim(x)[1] == dim(x)[2]){
+      inv <- NULL
+      set <- function(y) {
+        x <<- y
+        inv <<- NULL
+        }
+      get <- function() x
+      setInver <- function(solve) inv <<- solve
+      getInver <- function() inv
+      list(set = set, get = get,
+           setInver = setInver,
+           getInver = getInver)
   }
-  get <- function() x
-  setInver <- function(solve) inv <<- solve
-  getInver <- function() inv
-  list(set = set, get = get,
-       setInver = setInver,
-       getInver = getInver)
+  else{
+    message("Matrix is not invertible. Check dimensions")
+    NULL
+  }
 }
 
+#Solve or get inverse matrix
 cacheSolve <- function(x, ...) {
   inv <- x$getInver()
   if(!is.null(inv)) {
